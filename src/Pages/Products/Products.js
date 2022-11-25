fdbf2eb2033ea929d721033ae7a48b9ed6a31326
MD-Baki <bakimd77@gmail.com>
2022-11-26 01:28:01 +0600

    show all product

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductsCard from "./ProductsCard/ProductsCard";
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URI}/allProducts`)
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    });
+
     return (
         <div>
             <div className="h-[300px] overflow-hidden rounded-lg shadow-lg">
@@ -10,12 +18,14 @@ const Products = () => {
                     alt=""
                 />
             </div>
-            <div>
+            <div className="pb-10 px-5 lg:px-0">
                 <h2 className="text-2xl font-medium text-primary text-center py-6">
                     All Products
                 </h2>
-                <div className="grid grid-cols-3 gap-8">
-                    <ProductsCard />
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {products.map((product) => (
+                        <ProductsCard key={product._id} product={product} />
+                    ))}
                 </div>
             </div>
         </div>
--- src/Pages/Products/ProductsCard/ProductsCard.js
+++ src/Pages/Products/ProductsCard/ProductsCard.js
@@ -1,21 +1,52 @@
 import React from "react";
 import "./ProductsCard.css";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-const ProductsCard = () => {
+const ProductsCard = ({ product }) => {
+    const { productName, image, purchase, selling, location, details } =
+        product;
+
     return (
-        <div>
-            <div className="relative">
-                <img
-                    src="https://images.pexels.com/photos/11237829/pexels-photo-11237829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt=""
-                />
-                <div className="absolute top-[10px] right-0 shadow-lg">
+        <div className="border rounded-lg shadow-lg pl-5 py-5">
+            <div className="relative pr-5">
+                <img src={image} alt="" />
+                <div className="absolute top-0 right-0 shadow-lg">
                     <p className="mark bg-orange-600 text-white pr-3 pl-7 py-px lg:py-2  uppercase">
-                        used phone
+                        used
                     </p>
                 </div>
             </div>
-            <h2>Iphone 11</h2>
+            <div className="border-t mr-5">
+                <h2 className="text-lg font-medium text-center py-3">
+                    {productName}
+                </h2>
+                <div className="font-light pb-2">
+                    {details.length > 100 ? (
+                        <p>{details.slice(0, 100) + "..."}</p>
+                    ) : (
+                        <p className="text-justify">{details}</p>
+                    )}
+                </div>
+                <div className="flex flex-wrap justify-between gap-2">
+                    <p className="flex items-center gap-1">
+                        <FaMapMarkerAlt /> {location}
+                    </p>
+                    <p className="font-light">
+                        Price:
+                        <span className="line-through px-px">${purchase}</span>
+                        <span className="font-medium">${selling}</span>
+                    </p>
+                </div>
+
+                <div className="pt-2 grid grid-cols-2 gap-2">
+                    <button className="btn btn-outline btn-primary btn-sm text-xs btn-block">
+                        favorite
+                    </button>
+                    <button className="btn btn-primary btn-sm text-xs btn-block">
+                        favorite
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
